0bc02f17e25cc78a795e9041b674683db337ff46
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-03 01:05:06 +0900

    feat: firebase 설치 및 config 설정

--- src/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@mui/icons-material";
-declare module "@mui/icons-mateial/Headphones";
-declare module "@mui/icons-mateial/Headphones";
-declare module "@mui/icons-material/Add";
-declare module "@mui/icons-material/Mic";
