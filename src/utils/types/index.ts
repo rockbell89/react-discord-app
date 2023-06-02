d40e3f407b8fb904cbccf743abeadbf17725c950
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-03 01:06:02 +0900

    feat: redux toolkit 설치 및 user reducer 작성

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,15 +1,17 @@
 export type Message = {
   timestamp: any;
   message: string;
-  user: {
-    uid: string;
-    photo: string;
-    email: string;
-    displayName: string;
-  };
+  user: User;
 };
 
 export type Channel = {
   id: string;
   channelName: string;
+};
+
+export type User = {
+  uid: string;
+  photo: string;
+  email: string;
+  displayName: string;
 };
