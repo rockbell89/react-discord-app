727ce2a9b6ace3d4a53c107eb47dd8eff0016817
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-03 01:58:50 +0900

    feat: 구글 로그인 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import "./App.css";
+import Login from "./components/Login";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { login, logout } from "./store/userSlice";
 
 function App() {
+  // const user = useSelector((state) => state.user.user);
+  const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          login({
+            uid: user.uid,
+            photo: user.photoURL,
+            email: user.email,
+            displayName: user.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
+  console.log(user);
+
+  // 로그인 전
+  if (!user) {
+    return <Login />;
+  }
+
+  // 로그인 후
   return (
     <div className="App">
       <>
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,3 +4,6 @@ import userReducer from "./userSlice";
 export const store = configureStore({
   reducer: userReducer,
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
